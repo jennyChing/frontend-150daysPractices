c1e46be8f8b889f5788d74f4b32b13527005cd8b
jennyChing <jklife3@gmail.com>
2017-04-06 21:32:46 -0400

    use react to rebuild share dice game

--- day7_diceWithReact/dice/src/index.js
+++ day7_diceWithReact/dice/src/index.js
@@ -1,34 +1,135 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('dice_container')
-// );
+function Pip(props) {
+  	if (props.className !== "hide") {
+  	  return (
+	    <div className={props.className} id={props.id}></div>
+	  );
+  } else {
+      return (
+	    <div></div>
+	);
+  }
+}
 
-// class Board extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       cards: [],
-//     };
-//   }
-//   render() {
-//     return (
-//       <div id="dice_container">
-// 	    <div class="die_face"></div>
-// 	    <div class="die_face"></div>
-// 	  </div>
-//     );
-//   }
-// }
+class Game extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			dice1: 6,
+			dice2: 6,
+		}
+	}
+	handleClick(){
+		var roll1 = Math.floor(Math.random() * 6 + 1);
+		var roll2 = Math.floor(Math.random() * 6 + 1);
+		this.setState({dice1: roll1, dice2: roll2});
+		console.log(this.state.dice1);
+	}
+	renderPip(d, i) {
+	    var className = "pip pip_" + i;
+	    var id = "die_" + d + "_pip_" + i;
+	    return <Pip className={className} id={id}/>;
+	}
+	render() {
+		if (this.state.dice1 === 6){
+			return (
+			    <div id="title">
+			      <h1>Hey you! Give us roll!</h1>
+			        <div className="die_face" id="die_1">
+			          {this.renderPip(1, 1)}
+			          {this.renderPip(1, 2)}
+			          {this.renderPip(1, 3)}
+			          {this.renderPip(1, 5)}
+			          {this.renderPip(1, 6)}
+			          {this.renderPip(1, 7)}
+			        </div>
+			        <div className="button btn_up" id="shaker">
+			          <div className="btn_text" onClick={() => this.handleClick()}>Shake</div>
+			        </div>
+			    </div>
+			);
+		} else if (this.state.dice1 === 5){
+			return (
+			    <div id="title">
+			      <h1>Hey you! Give us roll!</h1>
+			        <div className="die_face" id="die_1">
+			          {this.renderPip(1, 1)}
+			          {this.renderPip(1, 3)}
+			          {this.renderPip(1, 4)}
+			          {this.renderPip(1, 5)}
+			          {this.renderPip(1, 7)}
+			        </div>
+			        <div className="button btn_up" id="shaker">
+			          <div className="btn_text" onClick={() => this.handleClick()}>Shake</div>
+			        </div>
+			    </div>
+			);
+		} else if (this.state.dice1 === 4){
+			return (
+			    <div id="title">
+			      <h1>Hey you! Give us roll!</h1>
+			        <div className="die_face" id="die_1">
+			          {this.renderPip(1, 1)}
+			          {this.renderPip(1, 3)}
+			          {this.renderPip(1, 5)}
+			          {this.renderPip(1, 7)}
+			        </div>
+			        <div className="button btn_up" id="shaker">
+			          <div className="btn_text" onClick={() => this.handleClick()}>Shake</div>
+			        </div>
+			    </div>
+			);
+		} else if (this.state.dice1 === 3){
+			return (
+			    <div id="title">
+			      <h1>Hey you! Give us roll!</h1>
+			        <div className="die_face" id="die_1">
+			          {this.renderPip(1, 1)}
+			          {this.renderPip(1, 4)}
+			          {this.renderPip(1, 7)}
+			        </div>
+			        <div className="button btn_up" id="shaker">
+			          <div className="btn_text" onClick={() => this.handleClick()}>Shake</div>
+			        </div>
+			    </div>
+			);
+		} else if (this.state.dice1 === 2){
+			return (
+			    <div id="title">
+			      <h1>Hey you! Give us roll!</h1>
+			        <div className="die_face" id="die_1">
+			          {this.renderPip(1, 1)}
+			          {this.renderPip(1, 7)}
+			        </div>
+			        <div className="button btn_up" id="shaker">
+			          <div className="btn_text" onClick={() => this.handleClick()}>Shake</div>
+			        </div>
+			    </div>
+			);
+		} else {
+			return (
+			    <div id="title">
+			      <h1>Hey you! Give us roll!</h1>
+			        <div className="die_face" id="die_1">
+			          {this.renderPip(1, 4)}
+			        </div>
+			        <div className="button btn_up" id="shaker">
+			          <div className="btn_text" onClick={() => this.handleClick()}>Shake</div>
+			        </div>
+			    </div>
+			);
+		}    
+  	}
+}
 
-// ReactDOM.render(
-//   <Board />,
-//   document.getElementById('dice_container')
-// );
+ReactDOM.render(
+  <Game />,
+  document.getElementById('dice_container')
+);
+
 
 // $(document).ready(function () {
 // 	$('.pip_4').hide();
